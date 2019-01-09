6e33798a689ce63b7581c2ffd444264fdac0aa35
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-01-09 15:20:03 +0100

    chore(*): setup test environment and CI-config

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='environment-variable-getter',
+    version='0.0.0',
+    packages=[''],
+    url='https://github.com/code-iai/environment-variable-getter',
+    license='MIT',
+    author='University of Bremen; Navid Jadid',
+    author_email='jadid.navid@yahoo.de',
+    description='A package to safely retrieve environment variables'
+)
