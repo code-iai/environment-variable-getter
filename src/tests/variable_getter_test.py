15649baede8a4cc5d530218a7e9010c59786053e
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-01-10 15:13:10 +0100

    test(*): add tests

---
+++ src/tests/variable_getter_test.py
@@ -0,0 +1,30 @@
+import pytest
+import os
+from .. import variable_getter as vg
+
+ENVIRONMENT_VARIABLE_NAME = 'ENV1'
+ENVIRONMENT_VARIABLE_VALUE = 'myval'
+ENVIRONMENT_VARIABLE_DEFAULT_VALUE = 'default'
+
+def test_successfully_get_required_variable(monkeypatch):
+    monkeypatch.setitem(os.environ, ENVIRONMENT_VARIABLE_NAME, ENVIRONMENT_VARIABLE_VALUE)
+    assert vg.get_required_variable(ENVIRONMENT_VARIABLE_NAME) == ENVIRONMENT_VARIABLE_VALUE
+
+def test_required_variable_exception():
+    with pytest.raises(KeyError, match=ENVIRONMENT_VARIABLE_NAME):
+        vg.get_required_variable(ENVIRONMENT_VARIABLE_NAME)
+
+def test_get_variable_with_default(monkeypatch):
+    monkeypatch.setitem(os.environ, ENVIRONMENT_VARIABLE_NAME, ENVIRONMENT_VARIABLE_VALUE)
+    assert vg.get_variable_with_default(
+        ENVIRONMENT_VARIABLE_NAME,
+        ENVIRONMENT_VARIABLE_DEFAULT_VALUE) == ENVIRONMENT_VARIABLE_VALUE
+
+def test_get_variable_without_default_exception():
+    with pytest.raises(TypeError):
+        vg.get_variable_with_default(ENVIRONMENT_VARIABLE_NAME)
+
+def test_get_default_value():
+    assert vg.get_variable_with_default(
+        ENVIRONMENT_VARIABLE_NAME,
+        ENVIRONMENT_VARIABLE_DEFAULT_VALUE) == ENVIRONMENT_VARIABLE_DEFAULT_VALUE
