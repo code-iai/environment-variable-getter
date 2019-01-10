510a038a7741d1f9d67002bff151115ea3687dec
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-01-10 15:12:13 +0100

    build(setup): update setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 setup(
     name='environment-variable-getter',
     version='0.0.0',
-    packages=[''],
     url='https://github.com/code-iai/environment-variable-getter',
     license='MIT',
     author='University of Bremen; Navid Jadid',
     author_email='jadid.navid@yahoo.de',
-    description='A package to safely retrieve environment variables'
+    description='A package to safely retrieve environment variables',
+    package_dir={'environment-variable-getter': 'src'},
+    packages=['environment-variable-getter'],
+    tests_require=['pytest']
 )
