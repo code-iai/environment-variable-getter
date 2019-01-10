7c5893f9017e3f9a49099ee8f2094dfc61a27e68
Navid M.-Jadid <jadid.navid@yahoo.de>
2019-01-10 15:30:42 +0100

    refactor: remove explicit raising of TypeError in method
    remove explicit raising of TypeError in get_variable_with_default
    because it is trivial and thrown during runtime anyway

--- src/variable_getter.py
+++ src/variable_getter.py
@@ -3,16 +3,12 @@ import os
 def get_required_variable(var_name):
     try:
         return os.environ[var_name]
-    except KeyError as err:
-        print(err)
-        raise
+    except KeyError:
+        raise KeyError('KeyError: environment variable {0} does not exist'.format(var_name))
 
 
 def get_variable_with_default(var_name, default):
     try:
         return get_required_variable(var_name)
-    except TypeError as err:
-        print(err)
-        raise
     except KeyError:
         return default
